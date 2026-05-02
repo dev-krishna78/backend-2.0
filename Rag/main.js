16b9a99d817b1720ad3304bf77a746f0a95b4faa
dev-krishna78 <krishnacarpenter222@gmail.com>
2026-05-02 17:16:21 +0530

    Integrate mitral embed model for convert chunks into embedding and also setup the dotenv for Api key or secret key

--- Rag/main.js
+++ Rag/main.js
@@ -1,6 +1,11 @@
+import dotenv from "dotenv"
+dotenv.config({ 
+    path: ['.env.local', '.env'] 
+});
+
 import { PDFParse } from "pdf-parse";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-
+import { MistralAIEmbeddings } from "@langchain/mistralai";
 import fs from "fs"
 
 let databuffer = fs.readFileSync("./SRS.pdf")
@@ -11,10 +16,18 @@ const parse = new PDFParse({
 
 const data = await parse.getText()
 
+const embeddings = new MistralAIEmbeddings({
+    apiKey:process.env.MISTRAL_API_KEY,
+    model: "mistral-embed"
+})
+
 const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 10,
+    chunkSize: 200,
     chunkOverlap: 0
 })
 
-const chunks = await splitter.splitText(data.text)
-console.log(chunks);
+const chunks = await splitter.splitText(data.text);
+
+const docs = await embeddings.embedDocuments(chunks);
+
+console.log(docs);
